6d2b4636e16087f89fe250c56fba864e8ce3dfb5
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-07-15 01:48:42 -0400

    Get singular deck and get all decks

--- server/src/controllers/getDeckController.ts
+++ server/src/controllers/getDeckController.ts
@@ -2,9 +2,11 @@ import { Request, Response } from "express";
 // Gives access to the deck model
 import Deck from "../models/deck";
 
-export async function getDecksController(req: Request, res: Response) {
+export async function getDeckController(req: Request, res: Response) {
+    const deckId = req.params.deckId;
     // Fetch the decks from MongoDB
-    const userDecks = await Deck.find();
+    const deck = await Deck.find({ id: deckId });
+
     // Send back array to the UI
-    res.json(userDecks);
+    res.status(200).json(deck);
 }
