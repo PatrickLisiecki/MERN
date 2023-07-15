42e9227179204f13bb162783cd327bb124053673
PatrickLisiecki <impatricklisiecki@gmail.com>
2023-07-15 01:47:28 -0400

    Created and styled error page

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,16 +3,19 @@ import ReactDOM from "react-dom/client";
 import App from "./App";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Deck from "./Deck";
+import ErrorPage from "./ErrorPage";
 
 //Whenever a user accesses the slash URL, it will display our React App
 const router = createBrowserRouter([
     {
         path: "/",
         element: <App />,
+        errorElement: <ErrorPage />,
     },
     {
         path: "/decks/:deckId",
         element: <Deck />,
+        errorElement: <ErrorPage />,
     },
 ]);
 
